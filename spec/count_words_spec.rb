818c8783ade62c036cacc75890a3fb4b51abb405
Graeme Paterson <ghpaterson@gmail.com>
2022-11-09 10:40:36 +0000

    updated count_words challenge

--- spec/count_words_spec.rb
+++ spec/count_words_spec.rb
@@ -1,6 +1,11 @@
 require 'count_words'
 
 RSpec.describe "count_words method" do
+    it "returns a count of 0 if string is empty" do
+        result = count_words(" ")
+        expect(result).to eq 0
+    end
+    
    it "returns a count of 1 if string is one word" do
         result = count_words("one")
         expect(result).to eq 1
